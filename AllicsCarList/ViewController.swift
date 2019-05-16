54c38e40f1f9d7de5d99f5a3f8555bc79772547f
andrewdonahue <donahue00@gmail.com>
2019-05-16 10:42:24 -0400

    web

--- AllicsCarList/ViewController.swift
+++ AllicsCarList/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate
+{
+    var webView: WKWebView!
+    
+    override func loadView()
+    {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.google.com/imghp?hl=en&tab=wi&authuser=0")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
 }
-
